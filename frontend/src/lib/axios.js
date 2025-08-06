efd577657823903440a34dc07ca539c446b60cfb
farehaaslam <farehaaslam57@gmail.com>
2025-08-06 21:39:07 +0530

    added backend url on frontend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
+const PROD_BACKEND_URL = "https://codetracker-api.onrender.com/api";
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE==="development" ?"http://localhost:5000/api":"/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5000/api"
+      : PROD_BACKEND_URL,
   withCredentials: true,
 });
 axiosInstance.interceptors.request.use(
@@ -23,23 +27,23 @@ axiosInstance.interceptors.response.use(
       originalRequest._retry = true;
 
       const refreshToken = localStorage.getItem("refreshToken");
-   //console.log("genrating new access")
+      //console.log("genrating new access")
       try {
-        const res = await axios.post("http://localhost:5000/api/user/refresh", {
-          refreshToken,
-        });
+        const res = await axios.post(
+          import.meta.env.MODE === "development"
+            ? "http://localhost:5000/api/user/refresh"
+            : "https://your-backend-service-name.onrender.com/api/user/refresh",
+          {
+            refreshToken,
+          }
+        );
 
         const newAccessToken = res.data.accessToken;
         localStorage.setItem("accessToken", newAccessToken);
 
-        // retry original request with new token
         originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
         return axiosInstance(originalRequest);
-      } catch (refreshErr) {
-       // console.log("Refresh token invalid or expired:", refreshErr);
-        // optionally logout user here
-       
-      }
+      } catch (refreshErr) {}
     }
 
     return Promise.reject(error);
